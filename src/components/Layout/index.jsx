6fbadfe4b9bdd586d5a082963e13819836bf65ab
yvbathia <yvbathia@gmail.com>
2020-03-29 19:11:04 +0530

    Added Fonts (you're doing great)

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import { Helmet } from "react-helmet";
 import PropTypes from "prop-types";
 import s from "./Layout.module.scss";
+import Helmet from "react-helmet";
 
 const propType = {
   children: PropTypes.node.isRequired,
@@ -11,9 +12,12 @@ const propType = {
 const Layout = ({ children, setSearch }) => {
   return (
     <div className={s.root}>
-      {/* <Helmet>
-        <link rel="stylesheet" href={`/fonts/fonts.css`} />
-      </Helmet> */}
+      <Helmet defer={false} htmlAttributes={{ lang: "en-US" }}>
+        <title>Gutendex</title>
+        <link rel="stylesheet" href={`/styles/fonts/fonts.css`} />
+        <meta name="description" content="Gutendex api example in react" />
+        <meta name="title" content="Gutendex" />
+      </Helmet>
       <div className={s.child}>{children}</div>
     </div>
   );
